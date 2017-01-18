3bcdcabc86a61cfe776c2acb9ee51b73631f1e03
lujjjh <lujjjh@gmail.com>
2017-01-18 16:28:38 +0800

    Use object mode as default

--- test/basic.js
+++ test/basic.js
@@ -4,55 +4,77 @@ var stream = require('stream');
 var Streamory = require('..');
 
 describe('Streamory', function () {
-  describe('normal mode', function () {
-    it('should write what it reads', function (done) {
-      var someChunk = 'Hello world!';
-      var source = new stream.Readable();
-      var writable = new stream.Writable({
-        write: function (chunk, encoding, callback) {
-          callback();
-          assert.equal(chunk, someChunk);
-          done();
-        }
-      });
-      source.pipe(new Streamory()).pipe(writable);
-      source.push(someChunk);
-      source.push(null);
+  it('should write what it reads', function (done) {
+    var someChunk = 'Hello world!';
+    var source = new stream.Readable();
+    var writable = new stream.Writable({
+      write: function (chunk, encoding, callback) {
+        callback();
+        assert.equal(chunk, someChunk);
+        done();
+      }
     });
+    source.pipe(new Streamory()).pipe(writable);
+    source.push(someChunk);
+    source.push(null);
+  });
 
-    it('should cache the last chunk', function (done) {
-      var someChunk = 'Hello world!';
-      var anotherChunk = 'Foo bar baz';
-      var tasks = [];
-      var source = new stream.Readable();
-      var streamory = new Streamory();
-      source.pipe(streamory);
+  it('should cache the last chunk', function (done) {
+    var someChunk = 'Hello world!';
+    var anotherChunk = 'Foo bar baz';
+    var tasks = [];
+    var source = new stream.Readable();
+    var streamory = new Streamory();
+    source.pipe(streamory);
+    tasks.push(streamory.get());
+    source.push(someChunk);
+    source.push(anotherChunk);
+    source.push(null);
+    setTimeout(function () {
       tasks.push(streamory.get());
-      source.push(someChunk);
-      source.push(anotherChunk);
-      source.push(null);
-      setTimeout(function () {
-        tasks.push(streamory.get());
-        Promise.all(tasks).then(function (results) {
-          assert.equal(results[0], someChunk);
-          assert.equal(results[1], anotherChunk);
-          done();
-        });
-      }, 0);
-    });
-
-    it('should return immediately if returnImmediately is truthy', function (done) {
-      var someChunk = 'Hello world!';
-      var streamory = new Streamory();
-      assert.strictEqual(streamory.get(true), null);
-      var source = new stream.Readable();
-      source.push(someChunk);
-      source.push(null);
-      source.pipe(streamory);
-      setTimeout(function () {
-        assert.equal(streamory.get(true), someChunk);
+      Promise.all(tasks).then(function (results) {
+        assert.equal(results[0], someChunk);
+        assert.equal(results[1], anotherChunk);
         done();
       });
+    }, 0);
+  });
+
+  it('should return immediately if returnImmediately is truthy', function (done) {
+    var someChunk = 'Hello world!';
+    var streamory = new Streamory();
+    assert.strictEqual(streamory.get(true), null);
+    var source = new stream.Readable();
+    source.push(someChunk);
+    source.push(null);
+    source.pipe(streamory);
+    setTimeout(function () {
+      assert.equal(streamory.get(true), someChunk);
+      done();
+    });
+  });
+
+  it('should work with plain object', function (done) {
+    var someObject = { foo: 'bar' };
+    var source = new stream.PassThrough({ objectMode: true });
+    var streamory = new Streamory();
+    source.pipe(streamory);
+    source.write(someObject);
+    streamory.get().then(function (data) {
+      assert.equal(data, someObject);
+      done();
+    });
+  });
+
+  it('should work with falsy value', function (done) {
+    var someValue = false;
+    var source = new stream.PassThrough({ objectMode: true });
+    var streamory = new Streamory();
+    source.pipe(streamory);
+    source.write(someValue);
+    streamory.get().then(function (data) {
+      assert.equal(data, someValue);
+      done();
     });
   });
 });
